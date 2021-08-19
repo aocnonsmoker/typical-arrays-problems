ca78f8679be3a8f6f155ee8e3846f429f9bede03
Zhambyl Kairken <z.kairken@csi.kz>
2021-08-19 19:15:44 +0600

    min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minNumber = 0;
+  if (array === undefined || array.length === 0) {
+    return minNumber;
+  }
+  array.forEach((number) => {
+    if (minNumber >= number) {
+      minNumber = number
+    }
+  })
+  return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxNumber = 0;
+  if (array === undefined || array.length === 0) {
+    return maxNumber;
+  }
+  array.forEach((number) => {
+    if (number >= maxNumber) {
+      maxNumber = number;
+    }
+  })
+  return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let average = 0;
+  if (array === undefined || array.length === 0) {
+    return average;
+  }
+  array.forEach((number) => {
+    average += number;
+  });
+  return average / array.length;
 }
